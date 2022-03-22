13eca760d343266536dd2a1a00ccda5b34bb2992
Irene-Busah <i.busah@alustudent.com>
2022-03-22 18:19:03 +0200

    new update

---
+++ public/contact.php
@@ -0,0 +1,17 @@
+<?php
+    if (isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $mailFrom = $_POST['email'];
+        $country = $_POST['country'];
+        $message = $_POST['message'];
+    
+        $mailTo = "i.busah123@gmail.com";
+        $headers = "From: ".$mailFrom;
+        $text = "You have a message ".$name."\n\n".$message;
+    
+        mail($mailTo, $subject, $text, $headers);
+    
+        header("Location: ./index.html?MessageSent");
+        
+    }
+?>
